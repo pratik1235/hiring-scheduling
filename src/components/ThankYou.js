4261610f10f3d6ef7aaedc476afe387b0ae9b70c
sbaidWork <dark12arrow@gmail.com>
2020-05-26 12:08:56 +0530

    made some UI corrections

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -10,7 +10,6 @@ class ThankYou extends Component{
     render(){
         return(
             <div className="container">
-                <Header/>
                 <div className="row"><p></p></div>
                 <div className="valign-wrapper"><Link to={link}><h3 className=" center red-text text-lighten-1">Thank You for your time!!</h3></Link></div>
             </div>
