d10df20b4acbe8e59fca83a46f4d6e8ecd34360c
sbaidWork <dark12arrow@gmail.com>
2020-05-31 19:08:18 +0530

    typo corrected

--- src/components/AlreadySubmitted.js
+++ src/components/AlreadySubmitted.js
@@ -11,7 +11,7 @@ class AlreadySubmitted extends Component{
             <div className="container">
                 <div className="row"><p></p></div>
                 <div className="valign-wrapper"><Link to={link}><h3 className=" center red-text text-lighten-1">You Have already submitted your response!</h3></Link></div>
-                <div className="row"><p>We have already received your response. No further changes can be made viad this form. Contact your hiring represntative for more information</p></div>
+                <div className="row"><p>We have already received your response. No further changes can be made via this form. Contact your hiring represntative for more information</p></div>
             </div>
         );
 
