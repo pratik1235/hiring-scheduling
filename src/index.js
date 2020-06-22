a2a9ba6d6ba52534dc6839ad64940a1c3c913701
pratik1235 <49610678+pratik1235@users.noreply.github.com>
2020-06-23 01:02:18 +0530

    updated index.js faux

--- src/index.js
+++ src/index.js
@@ -14,3 +14,6 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+///////inddex.js needs changes
